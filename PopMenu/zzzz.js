4ab852397f3c0fe4dafd4ae75af396e78231ec24
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-01-24 19:54:02 -0500

    autosave

--- PopMenu/zzzz.js
+++ PopMenu/zzzz.js
@@ -1,6 +1,17 @@
 for (let i = 0; i < document.getElementsByClassName('nav-btn-next').length; i++) {
     let cloned = document.getElementsByClassName('nav-btn-next')[i].cloneNode(true)
     cloned.classList.add('cloned-btn-next')
-    cloned.style = "float: right; "
+    cloned.style = "float: right;"
     document.getElementsByClassName('mktoButtonWrap')[i].appendChild(cloned)
+
+    Array.from(document.querySelectorAll('.nav-btn-prev')).map(x => x.innerText = '')
+    Array.from(document.querySelectorAll('.cloned-btn-next')).map(x => x.innerText = '')
 }
+
+
+function removeLastBtn() {
+    let numRows = Array.from(document.querySelectorAll('.mktoButtonRow')).length
+    Array.from(document.querySelectorAll('.mktoButtonRow'))[numRows - 1].classList.add('lastRow')
+}
+
+removeLastBtn()
