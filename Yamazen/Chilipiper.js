273b1c06f8c79333e952d2ab88f4f83320549c8e
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-02-03 14:49:21 -0500

    autosave

--- Yamazen/Chilipiper.js
+++ Yamazen/Chilipiper.js
@@ -1,3 +1,25 @@
 function q(a) { return function() { ChiliPiper[a].q = (ChiliPiper[a].q || []).concat([arguments]) } }
 window.ChiliPiper = window.ChiliPiper || "submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a, b) { a[b] = q(b); return a }, {});
 ChiliPiper.scheduling("account", "router")
+
+
+// 
+
+
+
+function q(a) { return function() { ChiliPiper[a].q = (ChiliPiper[a].q || []).concat([arguments]) } }
+window.ChiliPiper = window.ChiliPiper || "submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a, b) { a[b] = q(b); return a }, {});
+
+document.addEventListener('DOMContentLoaded', () => { ChiliPiper.scheduling("yamazen", "inbound-router", { title: "Thanks! What time works best for a quick call?" }) })
+
+
+///
+
+
+// 
+
+function q(a) { return function() { ChiliPiper[a].q = (ChiliPiper[a].q || []).concat([arguments]) } }
+window.ChiliPiper = window.ChiliPiper || "submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a, b) { a[b] = q(b); return a }, {});
+ChiliPiper.scheduling("yamazen", "router", {
+    map: true
+})
