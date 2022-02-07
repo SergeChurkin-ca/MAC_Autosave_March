6a5a78886378b7c017e9a13bf6092b9d2846b41f
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-02-07 10:04:27 -0500

    autosave

---
+++ WebSite/InternalUpdate/Page-2-test/js/script.js
@@ -0,0 +1,227 @@
+
+$(document).ready(function () {
+
+    document.getElementById('home').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink1').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "#fff";
+        document.getElementById("home").style.background = "none";
+        document.getElementById("navLink1").style.color = "red";
+        document.getElementById("navLink1").style.background = "#fff";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink2').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "#fff";
+        document.getElementById("home").style.background = "none";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "red";
+        document.getElementById("navLink2").style.background = "#fff";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink3').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "#fff";
+        document.getElementById("home").style.background = "none";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "red";
+        document.getElementById("navLink3").style.background = "#fff";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink4').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "#fff";
+        document.getElementById("home").style.background = "none";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "red";
+        document.getElementById("navLink4").style.background = "#fff";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+    document.getElementById('navLink5').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "#fff";
+        document.getElementById("home").style.background = "none";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "red";
+        document.getElementById("navLink5").style.background = "#fff";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink6').addEventListener('mouseover', function () {
+        document.getElementById("home").style.color = "#fff";
+        document.getElementById("home").style.background = "none";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "red";
+        document.getElementById("navLink6").style.background = "#fff";
+    })
+
+
+
+    document.getElementById('navLink1').addEventListener('mouseout', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink2').addEventListener('mouseout', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink3').addEventListener('mouseout', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink4').addEventListener('mouseout', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+    document.getElementById('navLink5').addEventListener('mouseout', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+    document.getElementById('navLink6').addEventListener('mouseout', function () {
+        document.getElementById("home").style.color = "red";
+        document.getElementById("home").style.background = "#fff";
+        document.getElementById("navLink1").style.color = "#fff";
+        document.getElementById("navLink1").style.background = "none";
+        document.getElementById("navLink2").style.color = "#fff";
+        document.getElementById("navLink2").style.background = "none";
+        document.getElementById("navLink3").style.color = "#fff";
+        document.getElementById("navLink3").style.background = "none";
+        document.getElementById("navLink4").style.color = "#fff";
+        document.getElementById("navLink4").style.background = "none";
+        document.getElementById("navLink5").style.color = "#fff";
+        document.getElementById("navLink5").style.background = "none";
+        document.getElementById("navLink6").style.color = "#fff";
+        document.getElementById("navLink6").style.background = "none";
+    })
+
+
+
+});
