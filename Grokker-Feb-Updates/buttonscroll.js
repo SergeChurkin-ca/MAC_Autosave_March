6d35bdb74fb9e4c1d120194654f0919b728e543f
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-02-18 13:01:27 -0500

    autosave

---
+++ Grokker-Feb-Updates/buttonscroll.js
@@ -0,0 +1,13 @@
+let scrollBtns = Array.from(document.querySelectorAll('button'))
+
+
+
+
+for (let i = 0; i < 2; i++) {
+    document.querySelectorAll('button')[i].addEventListener('click', myFunction)
+
+    function myFunction() {
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+
+}
