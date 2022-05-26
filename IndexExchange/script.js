611f9e7c4b2cdadb957b327c3c0722b5b489ca6b
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-05-26 11:30:27 -0400

    Autosave

--- IndexExchange/script.js
+++ IndexExchange/script.js
@@ -1,12 +1,15 @@
 MktoForms2.whenReady(function(form) {
-
-    console.log(Array.from(document.querySelectorAll('input')))
-
     const targetedInput = Array.from(document.querySelectorAll('input')).map(x => x.addEventListener('keydown', myFunction))
 
     function myFunction(e) {
-        console.log(e.target.parentNode.querySelectorAll('label'))
-        console.log(e.target.parentNode, " this is paretn node")
-        e.target.parentNode.querySelector('label').style = " top: -3px; transition: all 0.5s; font-size: 12px !important;"
+        console.log(e.target.type !== 'checkbox' || e.target.type !== 'hidden')
+
+
+        if (e.target.type !== 'checkbox' || e.target.type !== 'hidden') {
+            e.target.parentNode.querySelector('label').style = " top: -3px; transition: all 0.5s; font-size: 12px !important;"
+        }
+
+
+
     }
 })
