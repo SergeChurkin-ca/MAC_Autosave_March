18fa1a2d7b61a6e71ee0576899dff425cab0720f
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-05-30 13:45:21 -0400

    found an issue

---
+++ MAC-HubspotCenter/ver2/HubspotCenter-Test/script.js
@@ -0,0 +1,37 @@
+// toggling tabs
+const items = document.querySelectorAll(".item-title");
+
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active2");
+        current[0].parentNode.className = current[0].parentNode.className.replace("open", "closed");
+        current[0].className = current[0].className.replace(" active2", "");
+
+        console.log(current.length)
+
+        this.className += " active2";
+        this.parentNode.className = "open"
+
+    });
+}
+
+// arrow accordion 
+
+const containers = document.querySelectorAll(".list-row-1", ":after");
+
+containers.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("open");
+        item.previousElementSibling.classList.toggle("expand")
+    });
+});
+
+
+const items2 = document.querySelectorAll(".arrow-container");
+
+items2.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.previousElementSibling.classList.toggle("untoggled");
+        item.childNodes[0].classList.toggle('rotate')
+    });
+});
