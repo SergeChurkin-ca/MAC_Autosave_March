4a618af98a09576d0a688fd3379f7b8e3e4a7e01
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-08-09 15:46:48 -0400

    autosave

--- Linqia/Form-2/script.js
+++ Linqia/Form-2/script.js
@@ -4,5 +4,6 @@
 let errMessage = document.querySelectorAll('.error.no-label')
 let targetElement = document.querySelectorAll('.form-field.error')
 for (let i = 0; i < errMessage.length; i++) {
+
     targetElement[i].appendChild(errMessage[i])
 }
