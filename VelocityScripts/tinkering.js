00f73a7c68665cbfd8b3f43055a0edfbe2edc068
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-10-05 10:01:01 -0400

    autosave

--- VelocityScripts/tinkering.js
+++ VelocityScripts/tinkering.js
@@ -1,13 +1,24 @@
 ## $lead.Country
 #if ($lead.Country.equals("CN"))
-    #set ($billingCountry = "Billing Country: ${lead.Country}")
-    #elseif ($lead.Country.equals("United States"))
-    #set ($billingCountry = "Non Billing Country: ${lead.Country}")
+#set ($billingCountry = "Billing Country: ${lead.Country}")
+#elseif ($lead.Country.equals("United States"))
+#set ($billingCountry = "Non Billing Country: ${lead.Country}")
 #else (!$lead.Country.equals("1"))
-    #set($billingCountry = "Nothing here, did you add the country?")
+#set($billingCountry = "Nothing here, did you add the country?")
 #end
 <p>${billingCountry}</p>
 
-#if (${lead.Title} == "student") || ${lead.Title} == "Student"
-#set (<style>.main {display:none;}</style>)
+#if ($lead.Title.equals("student") || $lead.Title.equals("Student"))
+<table cellspacing="0" cellpadding="0" border="0" align="left">
+    <tbody>
+        <tr>
+            <td
+                style="text-align: center; text-decoration:none; font-size:18px;font-weight:bold; line-height:135%; font-family: 'Work Sans',Arial, sans-serif; border:1px solid #ffffff; background-color:#5c5c5c; border-radius: 10px;padding:5px;">
+                <a
+                    style="min-height:10px; text-decoration:none; color:#ffffff; padding: 9px 30px; display:inline-block"
+                    href="#">Conditional CTA</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
 #end
