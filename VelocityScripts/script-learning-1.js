b0c9993399f090b33f7fc6606abc2e19e97801e0
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-10-06 17:23:17 -0400

    autosave

---
+++ VelocityScripts/script-learning-1.js
@@ -0,0 +1,16 @@
+#set ( $organization_member_usageList = [
+{"plan_usage":0.35}
+])
+
+#set ( $actualPlannedNum = [{"plan_num": $lead.NumberOfEmployees}])
+
+#set ($percentage = 100.0 * $organization_member_usageList.get(0).plan_usage)
+
+#set ($plannedEmployeeNum = $actualPlannedNum.get(0).plan_num * 0.35 )
+
+## with decimal, e.g. 35.0%
+<p>$percentage%</p>
+## without decimal e.g. 35%
+<p>$percentage.intValue()%</p>
+
+<p>Planned Actual Number of Employees: $plannedEmployeeNum.intValue() of $lead.NumberOfEmployees</p>
--- VelocityScripts/script.js
+++
@@ -1,5 +0,0 @@
-#set($greeting = "test")
-${greeting}
-#if( $lead.med_cardiology.equals("1") )
-Cardiology##
-#end
