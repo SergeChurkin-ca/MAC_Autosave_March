67b0191fa8a9581ccef3a5d60dfc0bd8f8d08f3b
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-10-31 18:58:37 -0400

    autosave

--- EmpireCommunities/script.js
+++ EmpireCommunities/script.js
@@ -1,4 +1,6 @@
 const previousMasterField = document.querySelectorAll('.dependentField')[0].previousElementSibling.previousElementSibling;
 
 
-document.querySelectorAll('.dependentFieldSlave')[0].style.display == "none" ? previousMasterField.style = "width: calc((100% / 3 ) + 30% + 2rem) !important" : previousMasterField.style = "width: calc((100% / 3 ) - 1rem) !important";
+document.querySelectorAll('.dependentFieldSlave')[0].style.display !== "none" || document.querySelectorAll('.dependentFieldSlave')[0].style.display !== "" ? previousMasterField.style.width = " calc((100% / 3 ) - 1rem)" : previousMasterField.style.width = "calc((100% / 2 ) - 1rem)"
+
+console.log('check dynamic function: ', document.querySelectorAll('.dependentFieldSlave')[0].style.display)
