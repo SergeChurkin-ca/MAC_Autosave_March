468f3b54e3507c8268bed1053f585f69c310f8b4
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-12-12 14:46:54 -0500

    autosave

--- Acquire/carousel.js
+++ Acquire/carousel.js
@@ -1,18 +1,38 @@
 document.querySelectorAll('.owl-next')[0].addEventListener('click', carousleNext)
 document.querySelectorAll('.owl-prev')[0].addEventListener('click', carouslePrev)
-let owlStageContainer = document.querySelectorAll('.owl-stage')[0]
+
+let owlStageContainer = document.querySelector('.owl-stage')
+
+let outstageWidth = document.querySelector('.owl-stage-outer').offsetWidth;
+let owlconatiners = Array.from(document.querySelectorAll('.owl-item'))
 
 let total = 0
 
 function carousleNext() {
     total -= 296
-
     owlStageContainer.style = `transform: translate3d(calc(${total - 0 }px), 0px, 0px); transition: all 0.25s ease 0s;`
+
 }
-
-
 
 function carouslePrev() {
     total += 296
     owlStageContainer.style = `transform: translate3d(calc(${total - 0}px), 0px, 0px); transition: all 0.25s ease 0s;`
 }
+
+/*
+window.onresize = reportWindowSize;
+
+function reportWindowSize() {
+
+    owlconatiners.map((x) => x.offsetWidth = 900)
+}
+
+*/
+
+
+
+
+
+window.addEventListener("load", (event) => {
+    owlconatiners.map((x) => x.style = `width: calc((${outstageWidth}px / 3)) !important;`)
+});
