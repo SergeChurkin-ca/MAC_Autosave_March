eb48c70a3ebe77abd5fd4a65cf24835e0f31fcbf
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-12-13 10:07:16 -0500

    unstuck with cloned containers

--- Acquire/carousel.js
+++ Acquire/carousel.js
@@ -5,27 +5,13 @@ let owlStageContainer = document.querySelector('.owl-stage')
 
 let outstageWidth = document.querySelector('.owl-stage-outer').offsetWidth;
 let owlconatiners = Array.from(document.querySelectorAll('.owl-item'))
-
+let startingpoint = 0 - (owlconatiners[0].offsetWidth * 4)
 let total = 0
 
 function carousleNext() {
     total -= owlconatiners[0].offsetWidth
     owlStageContainer.style = `transform: translate3d(calc(${total - 0 }px), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
 
-    console.log('total is', total)
-
-    if (Array.from(document.querySelectorAll('.owl-item')).length <= 4) {
-        function duplicateChildNodes(parentId) {
-            let parent = document.getElementsByClassName('owl-stage')[0];
-            NodeList.prototype.forEach = Array.prototype.forEach;
-            let children = parent.childNodes;
-            children.forEach(function(item) {
-                let cln = item.cloneNode(true);
-                parent.appendChild(cln);
-            });
-        };
-        duplicateChildNodes();
-    }
 }
 
 function carouslePrev() {
@@ -33,23 +19,7 @@ function carouslePrev() {
     total += owlconatiners[0].offsetWidth
     owlStageContainer.style = `transform: translate3d(calc(${total - 0}px), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
 
-    if (Array.from(document.querySelectorAll('.owl-item')).length <= 8) {
-        function duplicateChildNodes(parentId) {
-            let parent = document.getElementsByClassName('owl-stage')[0];
-            NodeList.prototype.forEach = Array.prototype.forEach;
-            let children = parent.childNodes;
-            children.forEach(function(item) {
-                let cln = item.cloneNode(true);
-                // parent.appendChild(cln);
-                // console.log(parent.firstElementChild.innerHTML)
-                parent.insertBefore(cln, parent.firstElementChild)
 
-
-
-            });
-        };
-        duplicateChildNodes();
-    }
 }
 
 
@@ -57,35 +27,27 @@ function carouslePrev() {
 window.addEventListener("load", (event) => {
     owlconatiners.map((x) => x.style = `width: calc((${outstageWidth}px / 3)) !important;`)
 
+
+    let startingpoint = 0 - (owlconatiners[0].offsetWidth * 4)
+
+    function duplicateChildNodes(parentId) {
+        var parent = document.getElementsByClassName('owl-stage')[0];
+        NodeList.prototype.forEach = Array.prototype.forEach;
+        var children = parent.childNodes;
+        children.forEach(function(item) {
+            var cln = item.cloneNode(true);
+            parent.appendChild(cln);
+        });
+    };
+    duplicateChildNodes();
+    owlStageContainer.style = `transform: translate3d(calc(${startingpoint - 0 }px), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
+    console.log('totas is', startingpoint)
 });
 
 
 
 
-// function duplicateChildNodes(parentId) {
-//     var parent = document.getElementsByClassName('owl-stage')[0];
-//     NodeList.prototype.forEach = Array.prototype.forEach;
-//     var children = parent.childNodes;
-//     children.forEach(function(item) {
-//         var cln = item.cloneNode(true);
-//         parent.appendChild(cln);
-//     });
-// };
 
-// function duplicateChildNodes2(parentId) {
-//     var parent = document.getElementsByClassName('owl-stage')[0];
-//     NodeList.prototype.forEach = Array.prototype.forEach;
-//     var children = parent.childNodes;
-//     children.forEach(function(item) {
-//         var cln = item.cloneNode(true);
-//         parent.appendChild(cln);
-//     });
-// };
-
-
-
-// duplicateChildNodes();
-// duplicateChildNodes2();
 
 
 window.addEventListener('resize', windowResize)
@@ -94,3 +56,8 @@ function windowResize() {
     owlconatiners.map((x) => x.style = `width: calc((${outstageWidth}px / 3)) !important;border: solid 1px red;`)
 
 }
+
+// starting point i 1200
+
+
+// step is owlcontainer width
