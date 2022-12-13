bb3ebfe183d7a66941c3fa335e0260fcd58ee00a
SergeChurkin-ca <sergechurkin@yahoo.com>
2022-12-13 18:10:45 -0500

    autosave

--- Acquire/carousel.js
+++ Acquire/carousel.js
@@ -5,19 +5,19 @@ let owlStageContainer = document.querySelector('.owl-stage')
 
 let outstageWidth = document.querySelector('.owl-stage-outer').offsetWidth;
 let owlconatiners = Array.from(document.querySelectorAll('.owl-item'))
-let startingpoint = 0 - (owlconatiners[0].offsetWidth * 4)
-let total = -1292 // it is 4 sums of wol contaibaers
+let startingpoint = 0 - (owlconatiners[0].offsetWidth * 3)
+let total = 0 // it is 4 sums of wol contaibaers
 
 function carousleNext() {
     total -= owlconatiners[0].offsetWidth // this is step 
-    owlStageContainer.style = `transform: translate3d(calc(${total - 0}px ), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
+    owlStageContainer.style = `transform: translate3d(calc(${total}px ), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
     console.log.log(owlconatiners[0].offsetWidth)
 }
 
 function carouslePrev() {
 
     total += owlconatiners[0].offsetWidth
-    owlStageContainer.style = `transform: translate3d(calc(${total - 0}px), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
+    owlStageContainer.style = `transform: translate3d(calc(${total}px), 0px, 0px); transition: all 0.25s ease 0s;width:max-content;`
 
 
 }
