ee729c5db50ac374087580a8512ed2a55d5592a4
SergeChurkin-ca <sergechurkin@yahoo.com>
2023-01-12 15:03:19 -0500

    form funcitonlity

--- EDB/script.js
+++ EDB/script.js
@@ -1,5 +1,10 @@
-   Array.from(document.querySelectorAll('input')).map((x) => x.addEventListener('change', myFunc))
+   Array.from(document.querySelectorAll('input')).map((x) => x.addEventListener('change', myFunc));
+   Array.from(document.querySelectorAll('select')).map((x) => x.addEventListener('change', myFunc2));
 
    function myFunc() {
        Array.from(document.querySelectorAll('input')).map((x) => x.value !== '' ? x.parentElement.classList.add('activefield') : x.parentElement.classList.remove('activefield'))
+   }
+
+   function myFunc2(e) {
+       e.target.classLsit.add('activefield')
    }
