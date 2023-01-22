6c85a4a6fec00270d090291cc9d8af67407ada5f
SergeChurkin-ca <sergechurkin@yahoo.com>
2023-01-22 18:53:43 -0500

    autosave

--- Illumio/script.js
+++ Illumio/script.js
@@ -1,6 +1,12 @@
-// select only all updates
-MktoForms2.whenReady(function(form) {
+  document.querySelector('.nav-toggle-button').addEventListener("click", mobMenuToggle);
 
+  function mobMenuToggle(e) {
+      document.querySelectorAll('.primary-nav-wrapper')[0].classList.toggle("active")
+      console.log('hello')
+  }
 
-    document.querySelectorAll('.mktoLogicalField')[0].parentElement.classList.add('checkboxcontainer')
-});
+  Array.from(document.querySelectorAll('.open-filters')).map(x => x.addEventListener('click', subMenuToggle))
+
+  function subMenuToggle(e) {
+      e.target.parentNode.parentNode.nextElementSibling.classList.toggle("show")
+  }
