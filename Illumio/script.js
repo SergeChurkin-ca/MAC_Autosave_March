000d1e8c6f2977606fd0dfb696a1cd9175f3fc15
SergeChurkin-ca <sergechurkin@yahoo.com>
2023-01-23 00:37:26 -0500

    autosave

--- Illumio/script.js
+++ Illumio/script.js
@@ -6,20 +6,55 @@
   }
 
 
+  /*
 
+    const callToActionBtns = document.querySelectorAll('.open-filters');
+
+    callToActionBtns.forEach((btn) => {
+
+        btn.addEventListener("click", (e) => {
+
+            //   callToActionBtns.forEach(f => f.parentNode.nextElementSibling.classList.remove("show"));
+
+            if (!e.target.parentNode.parentNode.nextElementSibling.classList.contains("show")) {
+                e.target.parentNode.parentNode.nextElementSibling.classList.add("show");
+            } else if (e.target.parentNode.parentNode.nextElementSibling.classList.contains("show")) {
+                e.target.parentNode.parentNode.nextElementSibling.classList.remove("show");
+
+            }
+            //   console.log(e.target.parentNode.parentNode.nextElementSibling.classList.contains("show"))
+
+        });
+
+    });
+  */
+
+  //   Array.from(document.querySelectorAll('.fa-angle-down')).map(x => x.addEventListener('click', subMenuToggle))
+
+  //   function subMenuToggle(e) {
+  //       e.target.parentNode.parentNode.nextElementSibling.classList.remove("show")
+
+  //   }
 
   const callToActionBtns = document.querySelectorAll('.open-filters');
 
   callToActionBtns.forEach((btn) => {
       btn.addEventListener("click", (e) => {
-          callToActionBtns.forEach(f => f.parentNode.nextElementSibling.classList.remove("show"));
-          e.target.parentNode.parentNode.nextElementSibling.classList.toggle("show");
-          //   Array.from(document.querySelectorAll('.open-filters')).map(x => x.addEventListener('click', subMenuToggle))
+          // Get the siblings of the clicked element
+          const siblings = [...e.target.parentNode.parentNode.nextElementSibling.parentNode.children].filter(child => child !== e.target.parentNode.parentNode.nextElementSibling);
 
-          //   function subMenuToggle(e) {
-          //       e.target.parentNode.parentNode.nextElementSibling.classList.toggle("show")
+          // Iterate over the siblings and hide them
+          siblings.forEach(sibling => {
+              sibling.classList.remove("show");
+          });
 
-          //   }
-
+          // Check if the clicked element's next sibling has the class "show"
+          if (!e.target.parentNode.parentNode.nextElementSibling.classList.contains("show")) {
+              // If it doesn't, add the class "show"
+              e.target.parentNode.parentNode.nextElementSibling.classList.add("show");
+          } else if (e.target.parentNode.parentNode.nextElementSibling.classList.contains("show")) {
+              // If it does, remove the class "show"
+              e.target.parentNode.parentNode.nextElementSibling.classList.remove("show");
+          }
       });
   });
