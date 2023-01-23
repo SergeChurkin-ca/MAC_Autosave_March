c23b56f2c7df096e23f190711d9f5f8fc8cc56ca
SergeChurkin-ca <sergechurkin@yahoo.com>
2023-01-22 23:34:58 -0500

    sibling works, need to find how to foggle elemnt itself

--- Illumio/script.js
+++ Illumio/script.js
@@ -2,11 +2,24 @@
 
   function mobMenuToggle(e) {
       document.querySelectorAll('.primary-nav-wrapper')[0].classList.toggle("active")
-      Array.from(document.querySelectorAll('.primary-nav-wrapper')).mapx((x) => x.classList("active") == true ? x.classList.remove("active") : x.classList.toggle("active"))
+
   }
 
-  Array.from(document.querySelectorAll('.open-filters')).map(x => x.addEventListener('click', subMenuToggle))
 
-  function subMenuToggle(e) {
-      e.target.parentNode.parentNode.nextElementSibling.classList.toggle("show")
-  }
+
+
+  const callToActionBtns = document.querySelectorAll('.open-filters');
+
+  callToActionBtns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+          callToActionBtns.forEach(f => f.parentNode.nextElementSibling.classList.remove("show"));
+          e.target.parentNode.parentNode.nextElementSibling.classList.toggle("show");
+          //   Array.from(document.querySelectorAll('.open-filters')).map(x => x.addEventListener('click', subMenuToggle))
+
+          //   function subMenuToggle(e) {
+          //       e.target.parentNode.parentNode.nextElementSibling.classList.toggle("show")
+
+          //   }
+
+      });
+  });
